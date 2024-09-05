6028067c41550ca8ca71ea8badbbc6951d350cfc
David Flanagan <david@rawkode.dev>
2024-09-05 13:29:23 +0100

    fix(nodejs): lets just make the code stupid

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,16 +1,10 @@
 import { object, func, Directory } from "@dagger.io/dagger";
 import { Bun } from "./bun";
-import { Npm } from "./npm";
 
 @object()
 class nodejs {
   @func()
   bun(directory: Directory, version: string): Bun {
     return new Bun(directory, version);
-  }
-
-  @func()
-  npm(directory: Directory, version: string): Npm {
-    return new Npm(directory, version);
   }
 }
--- nodejs/src/npm.ts
+++
@@ -1,8 +0,0 @@
-import { object } from "@dagger.io/dagger";
-import { PackageManager } from "./packageManager";
-
-@object()
-export class Npm extends PackageManager {
-  protected image = "node";
-  protected executable = "npm";
-}
