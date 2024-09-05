ba5a4519dfcd358f48c7a84a39205fe78d6cabff
David Flanagan <david@rawkode.dev>
2024-09-05 11:33:08 +0100

    feat(nodejs): add npm and bun support

---
+++ nodejs/src/npm.ts
@@ -0,0 +1,39 @@
+import {
+	dag,
+	Container,
+	Directory,
+	object,
+	func,
+	CacheSharingMode,
+} from "@dagger.io/dagger";
+
+@object()
+class Bun {
+	private readonly version: string = "latest";
+
+	constructor(version: string) {
+		this.version = version;
+	}
+
+	/**
+	 * Install JavaScript/Typescript dependencies using npm.
+	 */
+	@func()
+	async install(directory: Directory): Promise<Container> {
+		const packageJson = directory.file("package.json");
+		const workingDirectory = "/code";
+		const packageName = `${
+			JSON.parse(await packageJson.contents()).name as string
+		}-node-modules`;
+
+		return dag
+			.container()
+			.from(`node:${this.version}`)
+			.withMountedDirectory(workingDirectory, directory)
+			.withWorkdir(workingDirectory)
+			.withMountedCache("/node_modules", dag.cacheVolume(packageName), {
+				sharing: CacheSharingMode.Shared,
+			})
+			.withExec(["npm", "install"]);
+	}
+}
