37401c03f793bfab2c80ddb49240afae2c778ec7
David Flanagan <david@rawkode.dev>
2024-09-05 11:46:57 +0100

    fix(nodejs): export bun and npm

--- nodejs/src/npm.ts
+++ nodejs/src/npm.ts
@@ -8,32 +8,32 @@ import {
 } from "@dagger.io/dagger";
 
 @object()
-class Bun {
-	private readonly version: string = "latest";
+export class Npm {
+  private readonly version: string = "latest";
 
-	constructor(version: string) {
-		this.version = version;
-	}
+  constructor(version: string) {
+    this.version = version;
+  }
 
-	/**
-	 * Install JavaScript/Typescript dependencies using npm.
-	 */
-	@func()
-	async install(directory: Directory): Promise<Container> {
-		const packageJson = directory.file("package.json");
-		const workingDirectory = "/code";
-		const packageName = `${
-			JSON.parse(await packageJson.contents()).name as string
-		}-node-modules`;
+  /**
+   * Install JavaScript/Typescript dependencies using npm.
+   */
+  @func()
+  async install(directory: Directory): Promise<Container> {
+    const packageJson = directory.file("package.json");
+    const workingDirectory = "/code";
+    const packageName = `${
+      JSON.parse(await packageJson.contents()).name as string
+    }-node-modules`;
 
-		return dag
-			.container()
-			.from(`node:${this.version}`)
-			.withMountedDirectory(workingDirectory, directory)
-			.withWorkdir(workingDirectory)
-			.withMountedCache("/node_modules", dag.cacheVolume(packageName), {
-				sharing: CacheSharingMode.Shared,
-			})
-			.withExec(["npm", "install"]);
-	}
+    return dag
+      .container()
+      .from(`node:${this.version}`)
+      .withMountedDirectory(workingDirectory, directory)
+      .withWorkdir(workingDirectory)
+      .withMountedCache("/node_modules", dag.cacheVolume(packageName), {
+        sharing: CacheSharingMode.Shared,
+      })
+      .withExec(["npm", "install"]);
+  }
 }
